af7829fcf4a51ae00df3c5c4c734e6e6c2d4b0cc
sashwathan <ssashwathan@gmail.com>
2024-08-22 14:28:34 +0530

    1st commit

---
+++ getting-started-with-threejs-main/index.js
@@ -0,0 +1,49 @@
+import * as THREE from "three";
+import { OrbitControls } from "jsm/controls/OrbitControls.js";
+
+const w = window.innerWidth;
+const h= window.innerHeight;
+
+const renderer = new THREE.WebGLRenderer({ antialias: true });
+renderer.setSize(w,h);
+document.body.appendChild(renderer.domElement);
+
+const fov = 75;
+const aspect = w/h;
+const near = 0.1;
+const far= 10;
+const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
+camera.position.z=2;
+const scene = new THREE.Scene();
+
+const controls = new OrbitControls(camera, renderer.domElement);
+controls.enableDamping = true;
+controls.dampingFactor= 1;
+
+const geo = new THREE.IcosahedronGeometry(1,5);
+const mat = new THREE.MeshStandardMaterial({
+    color: 0xffffff,
+    flatShading:true
+})
+const mesh = new THREE.Mesh(geo , mat);
+scene.add(mesh);
+
+const wireMat = new THREE.MeshBasicMaterial({
+    color: 0x87979789,
+    wireframe: true
+});
+
+const wireMesh = new THREE.Mesh(geo, wireMat);
+wireMesh.scale.setScalar(1.001);
+mesh.add(wireMesh);
+
+const hemiLight = new THREE.HemisphereLight(0x87000, 0x19a05);
+scene.add(hemiLight);
+
+function Animate(t=0){
+        requestAnimationFrame(Animate);
+        renderer.render(scene, camera);
+}
+
+Animate();
+console.log("hi")
